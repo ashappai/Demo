54f41f0abed9fa095c9de4df3086340add302dc7
asha <ashappai@gmail.com>
2021-06-18 17:56:51 +0530

    commit from US develop

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		System.out.println("This line is from USI tester");
+	    System.out.println("This line is from develop branch");
 	}
 
 }
